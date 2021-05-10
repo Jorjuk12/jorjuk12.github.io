2feeac73a09b43f22a00458af44a2c44a1255cc7
Jorjuk <83118686+Jorjuk@users.noreply.github.com>
2021-05-10 20:09:33 +0300

    1

---
+++ thanks.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			      <!-- Facebook Pixel Code -->
+ <img height="1" width="1" src="https://www.facebook.com/tr?id=<?php echo $_GET["pixel"];?>&ev=Lead&noscript=1"/>
+<!-- End Facebook Pixel Code -->
+				<meta charset="UTF-8" />
+				<title>CONGRATULATIONS! YOUR ORDER IS ACCEPTED!</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="stylesheet" href="css/style.css"/>
+				<link rel="stylesheet" href="css/media.css"/>
+			</head>
+			<body>
+				<div class="block1">
+					<div class="block_main">
+						<p class="text1">CONGRATULATIONS! YOUR ORDER IS ACCEPTED!</p>
+					  
+						<p class="text3">OUR OPERATOR WILL CONTACT YOU FOR CONFIRMATION</p>
+						<img class="operator" src="images/operator.png" alt="" />
+					</div>
+				</div>
+				
+	
+	
+	
+
+
+    
+
+
+
+
+	
+
+
+</body>
+			</html>
